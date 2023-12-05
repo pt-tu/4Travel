a4d3f347e111895b526c9b023b7a879561ca969f
tdyhoang <tdyhoang@gmail.com>
2023-12-05 22:04:44 +0700

    Update AddCustomer.tsx

--- src/pages/Task3/AddCustomer.tsx
+++ src/pages/Task3/AddCustomer.tsx
@@ -1,28 +1,40 @@
-import React from "react";
-import { DatePicker, Form, Input } from "antd";
+import "./AddCustomer.css";
+import { Col, Row, Button, DatePicker, Form, Input } from "antd";
 
 const { TextArea } = Input;
 
-const AddCustomer: React.FC = () => {
+function AddCustomer() {
   return (
     <div className="AddEdit">
       <h3>Quay lại</h3>
       <h1>THÔNG TIN KHÁCH HÀNG</h1>
-      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 14 }}>
-        <Form.Item name={"name"} required label="Họ và tên">
-          <Input />
-        </Form.Item>
-        <Form.Item name={"cic"} required label="CCCD">
-          <Input />
-        </Form.Item>
-        <Form.Item name={"tel"} required label="Số điện thoại">
-          <Input />
-        </Form.Item>
-        <Form.Item name={"email"} label="Email">
-          <Input />
-        </Form.Item>
+      <Form>
+        <Row>
+          <Col span={12}>
+            <Form.Item name={"name"} required label="Họ và tên">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item name={"cic"} required label="CMND/CCCD">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <Form.Item name={"phone"} required label="Số điện thoại">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item name={"email"} required label="Email">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
         <Form.Item name={"bday"} required label="Ngày sinh">
-          <DatePicker />
+          <DatePicker placeholder="DD/MM/YY" format={"DD/MM/YY"} />
         </Form.Item>
         <Form.Item name={"address"} required label="Địa chỉ">
           <Input />
@@ -33,9 +45,14 @@ const AddCustomer: React.FC = () => {
         <Form.Item name={"special"} label="Yêu cầu đặc biệt">
           <TextArea />
         </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
       </Form>
     </div>
   );
-};
+}
 
 export default AddCustomer;
