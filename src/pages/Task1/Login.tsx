3be6e68e51eff17451d2483800a409d97854dbd2
1609Dzuaa <106080372+1609Dzuaa@users.noreply.github.com>
2023-12-09 00:44:49 +0700

    initializing login form

--- src/pages/Task1/Login.tsx
+++ src/pages/Task1/Login.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React from 'react';
 
-function Login() {
-  return <div>Login</div>;
-}
+const Login = () => {
+    return (
+        <div className="login-container">
+            <form className="login-form">
+                <h2>Đăng Nhập</h2>
+                <label htmlFor="username">Tên Truy Cập:</label>
+                <input type="text" id="username" name="username" />
+
+                <label htmlFor="password">Mật Khẩu:</label>
+                <input type="password" id="password" name="password" />
+
+                <button type="submit">Đăng Nhập</button>
+            </form>
+        </div>
+    );
+};
 
 export default Login;
