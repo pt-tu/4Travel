5bb5b66ebc22bb2f693a34966e50011c47d61519
Thai Duong <thain6798@gmail.com>
2023-12-13 00:42:02 +0700

    Add Booking, History, CustomerList

---
+++ src/components/Task2Component/TourBooking.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./TourStyle.css"
+import Hanoi from "../../images/hanoi.jpg"
+export const TourBooking = () => {
+
+    return (
+        <div className="card GridContainer">
+            <div>
+                <img className="cardImg" src={Hanoi} alt="Hanoi" />
+            </div>
+            <div style={{ textAlign: "left", paddingLeft: 10 }}>
+                <h3 className="title">Combo 3N3Đ Hà Nội & Khách sạn 5 Sao (Bao ăn sáng, dịch vụ đưa đón tại sân bay)</h3>
+                <span className="normal">Mã tour: </span>
+                <span className="NameBold">NDDNG2640-011-161123VU</span> <br></br>
+                <span className="normal">Nơi khởi hành: </span>
+                <span className="NameBold">Đà Nẵng</span> <br></br>
+                <span className="normal">Khách sạn: </span>
+                <span className="NameBold">Peridot Grand Luxury Boutique Hotel</span> <br></br>
+            </div>
+            <div style={{ textAlign: "right", paddingRight: 10, paddingTop: 120 }}>
+                <span style={{color: "#FD5056"}} className="normal">Price: </span>
+                <span style={{color: "#FD5056"}} className="NameBold">6.999.000<u>đ</u></span>
+            </div>
+        </div>
+    )
+
+}
--- src/pages/Task2/TourManagement.tsx
+++ src/pages/Task2/TourManagement.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { TourManagementCard } from "../../components/Task2Component/TourManagementCard"
+import "../../components/Task2Component/TourStyle.css"
+import { Button } from 'antd';
+import { ArrowRightOutlined, FilterFilled, ReloadOutlined } from '@ant-design/icons';
+import { Input, Space, ConfigProvider, DatePicker, Select } from 'antd';
+
 
 function TourManagement() {
-  return <div>TourManagement</div>;
+
+  const { Search } = Input;
+
+  return <div>
+
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#4B268F',
+        },
+      }}
+    >
+      <div className="CenterContainer">
+
+        <Search placeholder="Nhập tên, mã tour hoặc khách hàng bạn muốn" className="SearchBar" allowClear enterButton size="large" />
+        <div style={{ width: '80%', display: 'flex', justifyContent: "space-between" }}>
+          <Button icon={<FilterFilled />} className="ButtonUp"> Lọc</Button>
+
+        </div>
+        <TourManagementCard></TourManagementCard>
+        <TourManagementCard></TourManagementCard>
+        <TourManagementCard></TourManagementCard>
+        <TourManagementCard></TourManagementCard>
+        <TourManagementCard></TourManagementCard>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', width: '90%', marginTop: 50 }}>
+        <Button type="primary" icon={<ArrowRightOutlined />} className="ButtonNext" style={{ direction: 'rtl', boxShadow: 'none' }}>
+          Xem thêm
+        </Button>
+      </div>
+    </ConfigProvider>
+  </div>;
 }
 
 export default TourManagement;
