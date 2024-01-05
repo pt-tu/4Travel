df1400a81348deab9853564e3108f5ecbee5f2da
tdyhoang <tdyhoang@gmail.com>
2024-01-05 21:55:03 +0700

    apply CUD to AddTour

--- src/pages/Task3/AddTour.tsx
+++ src/pages/Task3/AddTour.tsx
@@ -1,52 +1,153 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useState } from "react";
 import "./Form.css";
-import { Col, Row, Button, DatePicker, Form, Input, Select } from "antd";
+import useCreateTour from "../../hooks/TourManagement/useCreateTour";
+import {
+  Col,
+  Row,
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  Select,
+  Upload,
+  message,
+} from "antd";
+import { ArrowLeftOutlined, PlusOutlined } from "@ant-design/icons";
 
 const { TextArea } = Input;
 
 function AddTour() {
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  const [name, setname] = useState("");
+  const [tourguide_id, settourguide_id] = useState("");
+  const [bia, setbia] = useState<Blob | null>(null);
+  const [diemdi, setdiemdi] = useState("");
+  const [diemden, setdiemden] = useState("");
+  const [hotel, sethotel] = useState("");
+  const [start, setstart] = useState("");
+  const [end, setend] = useState("");
+  const [chitiet, setchitiet] = useState("");
+
+  const createTour = useCreateTour(
+    {
+      name: name,
+      tourguide_id: tourguide_id,
+      bia: bia,
+      diemdi: diemdi,
+      diemden: diemden,
+      hotel: hotel,
+      start: start,
+      end: end,
+      chitiet: chitiet,
+    },
+    id ?? ""
+  );
+
+  if (createTour.isSuccess) {
+    message.success("Thêm thành công");
+    navigate(-1);
+    createTour.reset();
+  } else if (createTour.error instanceof Error) {
+    message.error("Thêm thất bại. Lỗi: " + createTour.error.message);
+  }
+
+  const handleImageChange = (e: any) => {
+    const file = e.file;
+    setbia(file.originFileObj);
+  };
+
+  // prettier-ignore
+  const diaDiem = ["An Giang", "Bà Rịa – Vũng Tàu", "Bắc Giang", "Bắc Kạn", "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Dương", "Bình Phước", "Bình Thuận", "Cà Mau", "Cần Thơ", "Cao Bằng", "Đà Nẵng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Nội", "Hà Tĩnh", "Hải Dương", "Hải Phòng", "Hậu Giang", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Phú Yên", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thành phố Hồ Chí Minh", "Thừa Thiên Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái"];
+  const { Option } = Select;
+  
   return (
     <div className="wrapper">
-      <h3>Quay lại</h3>
+      <Button
+        type="text"
+        icon={<ArrowLeftOutlined />}
+        className="ButtonUp"
+        onClick={() => navigate(-1)}
+      >
+        {" "}
+        <b>Quay lại</b>
+      </Button>
+
       <h1 className="pageTitle">Thông tin tour</h1>
-      <Form labelCol={{ span: 3 }} labelAlign="left">
+
+      <Form
+        labelCol={{ span: 3 }}
+        labelAlign="left"
+        onFinish={() => createTour.mutate()}
+      >
         <Form.Item
           name={"name"}
           label="Tên tour"
           rules={[{ required: true, message: "Nhập tên tour" }]}
         >
-          <Input />
+          <Input onChange={(e) => setname(e.target.value)} />
         </Form.Item>
+
         <Form.Item
           name={"tgid"}
           label="Mã hướng dẫn viên"
           rules={[{ required: true, message: "Nhập mã HDV" }]}
         >
-          <Input className="shortInput" />
+          <Input
+            className="shortInput"
+            onChange={(e) => settourguide_id(e.target.value)}
+          />
         </Form.Item>
+
         <Form.Item name={"tgname"} label="Tên hướng dẫn viên">
           <Input disabled className="shortInput" />
         </Form.Item>
+
         <Form.Item
           name={"origin"}
           label="Điểm đi"
           rules={[{ required: true, message: "Nhập điểm đi" }]}
         >
-          <Select placeholder="Địa điểm" />
+          <Select
+            showSearch
+            placeholder="Địa điểm"
+            onChange={(e) => setdiemdi(e)}
+          >
+            {diaDiem.map((e) => (
+              <Option key={e}>{e}</Option>
+            ))}
+          </Select>
         </Form.Item>
+
         <Form.Item
           name={"destination"}
           label="Điểm đến"
           rules={[{ required: true, message: "Nhập điểm đến" }]}
         >
-          <Select placeholder="Địa điểm" />
+          <Select
+            showSearch
+            placeholder="Địa điểm"
+            onChange={(e) => setdiemden(e)}
+          >
+            {diaDiem.map((e) => (
+              <Option key={e}>{e}</Option>
+            ))}
+          </Select>
         </Form.Item>
+
         <Form.Item
           name={"hotel"}
           label="Khách sạn"
           rules={[{ required: true, message: "Nhập khách sạn" }]}
         >
-          <Input className="shortInput" />
+          <Input
+            className="shortInput"
+            onChange={(e) => sethotel(e.target.value)}
+          />
         </Form.Item>
+
         <Row>
           <Col span={12}>
             <Form.Item
@@ -55,9 +156,14 @@ function AddTour() {
               label="Thời gian bắt đầu"
               rules={[{ required: true, message: "Nhập thời gian bắt đầu" }]}
             >
-              <DatePicker placeholder="DD/MM/YY" format={"DD/MM/YY"} />
+              <DatePicker
+                placeholder="DD/MM/YY"
+                format={"DD/MM/YY"}
+                onChange={(e) => setstart(e?.toISOString() ?? "")}
+              />
             </Form.Item>
           </Col>
+
           <Col span={12}>
             <Form.Item
               labelCol={{ span: 6 }}
@@ -65,20 +171,48 @@ function AddTour() {
               label="Thời gian kết thúc"
               rules={[{ required: true, message: "Nhập thời gian kết thúc" }]}
             >
-              <DatePicker placeholder="DD/MM/YY" format={"DD/MM/YY"} />
+              <DatePicker
+                placeholder="DD/MM/YY"
+                format={"DD/MM/YY"}
+                onChange={(e) => setend(e?.toISOString() ?? "")}
+              />
             </Form.Item>
           </Col>
         </Row>
+
         <Form.Item name={"note"} label="Ghi chú">
-          <TextArea rows={5} />
+          <TextArea rows={5} onChange={(e) => setchitiet(e.target.value)} />
         </Form.Item>
+
+        <Form.Item name={"bia"} label="Ảnh bìa">
+          <Upload
+            name="bia"
+            listType="picture-card"
+            accept=".jpg"
+            showUploadList={false}
+            onChange={handleImageChange}
+          >
+            {bia != null ? (
+              <img
+                src={URL.createObjectURL(bia)}
+                alt="Ảnh bìa tour"
+                style={{ width: "100%", height: "100%" }}
+              />
+            ) : (
+              <div>
+                <PlusOutlined />
+                <div>Upload</div>
+              </div>
+            )}
+          </Upload>
+        </Form.Item>
+
         <Form.Item className="submitButton">
           <Button type="primary" htmlType="submit">
             Xác nhận
           </Button>
         </Form.Item>
       </Form>
-      {/*thêm upload ảnh tour*/}
     </div>
   );
 }
