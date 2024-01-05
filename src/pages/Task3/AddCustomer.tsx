3ebc0e8fa59309a3c8d29914d38a40ffd4e14b34
tdyhoang <tdyhoang@gmail.com>
2024-01-05 21:55:47 +0700

    fix AddCustomer (input ngaysinh)

--- src/pages/Task3/AddCustomer.tsx
+++ src/pages/Task3/AddCustomer.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import useCreateCustomer from "../../hooks/CustomerManagement/useCreateCustomer";
 import "./Form.css";
 import { Col, Row, Button, DatePicker, Form, Input, message } from "antd";
@@ -11,6 +11,7 @@ function AddCustomer() {
   const [form] = Form.useForm();
   const navigate = useNavigate();
 
+  const { id } = useParams();
   const [hoten, sethoten] = useState("");
   const [cccd, setcccd] = useState("");
   const [sdt, setsdt] = useState("");
@@ -31,15 +32,15 @@ function AddCustomer() {
       ghichu: ghichu,
       yeucau: yeucau,
     },
-    ""
+    id ?? ""
   );
 
   if (createCustomer.isSuccess) {
     message.success("Thêm thành công");
-    navigate("/danh-sach-khach-hang");
+    navigate(-1);
     createCustomer.reset();
   } else if (createCustomer.error instanceof Error) {
-    message.error(createCustomer.error.message);
+    message.error("Thêm thất bại. Lỗi: " + createCustomer.error.message);
   }
 
   return (
@@ -53,7 +54,9 @@ function AddCustomer() {
         {" "}
         <b>Quay lại</b>
       </Button>
+
       <h1 className="pageTitle">Thông tin khách hàng</h1>
+
       <Form
         form={form}
         labelCol={{ span: 3 }}
@@ -72,6 +75,7 @@ function AddCustomer() {
               <Input onChange={(e) => sethoten(e.target.value)} />
             </Form.Item>
           </Col>
+
           <Col span={11}>
             <Form.Item
               labelCol={{ span: 6 }}
@@ -83,6 +87,7 @@ function AddCustomer() {
             </Form.Item>
           </Col>
         </Row>
+
         <Row justify={"space-between"}>
           <Col span={12}>
             <Form.Item
@@ -95,19 +100,26 @@ function AddCustomer() {
               <Input onChange={(e) => setsdt(e.target.value)} />
             </Form.Item>
           </Col>
+
           <Col span={11}>
             <Form.Item labelCol={{ span: 6 }} name={"email"} label="Email">
               <Input onChange={(e) => setemail(e.target.value)} />
             </Form.Item>
           </Col>
         </Row>
+
         <Form.Item
           name={"bday"}
           label="Ngày sinh"
           rules={[{ required: true, message: "Nhập ngày sinh" }]}
         >
-          <DatePicker placeholder="DD/MM/YY" format={"DD/MM/YY"} />
+          <DatePicker
+            placeholder="DD/MM/YY"
+            format={"DD/MM/YY"}
+            onChange={(e) => setngaysinh(e?.toISOString() ?? "")}
+          />
         </Form.Item>
+
         <Form.Item
           name={"address"}
           label="Địa chỉ"
@@ -118,12 +130,15 @@ function AddCustomer() {
             onChange={(e) => setdiachi(e.target.value)}
           />
         </Form.Item>
+
         <Form.Item name={"notes"} label="Ghi chú">
           <TextArea rows={5} onChange={(e) => setghichu(e.target.value)} />
         </Form.Item>
+
         <Form.Item name={"adhocreq"} label="Yêu cầu đặc biệt">
           <TextArea rows={5} onChange={(e) => setyeucau(e.target.value)} />
         </Form.Item>
+
         <Form.Item className="submitButton">
           <Button type="primary" htmlType="submit">
             Xác nhận
