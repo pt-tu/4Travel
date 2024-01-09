c32ceb541c2c1643e65b7ab62d712c893543e05f
sakagawakyohei <lamgiabao1039@gmail.com>
2024-01-09 20:12:20 +0700

    export bill

--- src/pages/ThanhToan/Bill.tsx
+++ src/pages/ThanhToan/Bill.tsx
@@ -8,6 +8,7 @@ import useUser from "../../hooks/accountsystem/useUser";
 import BookingDaThanhToan from "./BookingDaThanhToan";
 import useCheckOut from "../../hooks/Bill/useCheckOut";
 import { v4 as uuidv4 } from "uuid";
+import generatePDF, { Margin, Resolution } from "react-to-pdf";
 
 const invoiceStyles: React.CSSProperties = {
   width: "500px",
@@ -130,103 +131,151 @@ const Bill: React.FC = () => {
       checkout.mutate();
     }
   }, [tour.data, cus.data]); //bug khi reload
+
+  const options = {
+    // default is `save`
+
+    method: "open",
+    // default is Resolution.MEDIUM = 3, which should be enough, higher values
+    // increases the image quality but also the size of the PDF, so be careful
+    // using values higher than 10 when having multiple pages generated, it
+    // might cause the page to crash or hang.
+    resolution: Resolution.HIGH,
+    align: "center",
+    page: {
+      // margin is in MM, default is Margin.NONE = 0
+      margin: Margin.SMALL,
+      // default is 'A4'
+      format: "letter",
+      // default is 'portrait'
+      orientation: "landscape",
+    },
+    canvas: {
+      // default is 'image/jpeg' for better size performance
+      mimeType: "image/png",
+      qualityRatio: 1,
+    },
+    // Customize any value passed to the jsPDF instance and html2canvas
+    // function. You probably will not need this and things can break,
+    // so use with caution.
+    overrides: {
+      // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
+      pdf: {
+        compress: true,
+      },
+      // see https://html2canvas.hertzen.com/configuration for more options
+      canvas: {
+        useCORS: true,
+      },
+    },
+  };
+  const getTargetElement = () => document.getElementById("content-id");
+
   return (
-    <div style={invoiceStyles}>
-      <h1
-        style={{
-          textAlign: "start",
-          marginTop: 20,
-          marginLeft: 30,
-          paddingBottom: 10,
-        }}
+    <>
+      <button
+        style={{ marginTop: 20 }}
+        onClick={() => generatePDF(getTargetElement as any, options as any)}
       >
-        4T
-      </h1>
-      <div style={headerStyles}>
-        <h2 style={{ marginBottom: 0, color: "#4B268F" }}> HÓA ĐƠN</h2>
-      </div>
-      <p style={text2}>Tên tour: {name} </p>
-      <Flex>
-        <div style={{ width: "50%" }}>
-          {/* Red section content */}
-          <div>
-            <p
-              style={{
-                fontSize: 16,
-                paddingBottom: 15,
-                textAlign: "start",
-                margin: 0,
-                paddingLeft: 18,
-                color: "#4B268F",
-                fontWeight: "bold",
-              }}
-            >
-              Đến:
-            </p>
-            <p style={text}>Ông/Bà: {hoten}</p>
-            <p style={text}>Số điện thoại: {sdt}</p>
-            <p style={text}>Email: {email}</p>
-          </div>
+        XUẤT BILL
+      </button>
+      <div style={invoiceStyles} id="content-id">
+        <h1
+          style={{
+            textAlign: "start",
+            marginTop: 20,
+            marginLeft: 30,
+            paddingBottom: 10,
+          }}
+        >
+          4T
+        </h1>
+        <div style={headerStyles}>
+          <h2 style={{ marginBottom: 0, color: "#4B268F" }}> HÓA ĐƠN</h2>
         </div>
-        <div style={{ width: "50%" }}>
-          {/* Blue section content */}
-          <div>
-            <p
-              style={{
-                fontSize: 16,
-                paddingBottom: 15,
-                textAlign: "start",
-                margin: 0,
-                color: "#4B268F",
-                fontWeight: "bold",
-                paddingLeft: 18,
-              }}
-            >
-              Chi tiết hóa đơn:
-            </p>
-            <p style={text}>Vào lúc: {formattedTime} </p>
-            <p style={text}>Bởi: {user.data?.user_metadata.ten}</p>
+        <p style={text2}>Tên tour: {name} </p>
+        <Flex>
+          <div style={{ width: "50%" }}>
+            {/* Red section content */}
+            <div>
+              <p
+                style={{
+                  fontSize: 16,
+                  paddingBottom: 15,
+                  textAlign: "start",
+                  margin: 0,
+                  paddingLeft: 18,
+                  color: "#4B268F",
+                  fontWeight: "bold",
+                }}
+              >
+                Đến:
+              </p>
+              <p style={text}>Ông/Bà: {hoten}</p>
+              <p style={text}>Số điện thoại: {sdt}</p>
+              <p style={text}>Email: {email}</p>
+            </div>
           </div>
-        </div>
-      </Flex>
+          <div style={{ width: "50%" }}>
+            {/* Blue section content */}
+            <div>
+              <p
+                style={{
+                  fontSize: 16,
+                  paddingBottom: 15,
+                  textAlign: "start",
+                  margin: 0,
+                  color: "#4B268F",
+                  fontWeight: "bold",
+                  paddingLeft: 18,
+                }}
+              >
+                Chi tiết hóa đơn:
+              </p>
+              <p style={text}>Vào lúc: {formattedTime} </p>
+              <p style={text}>Bởi: {user.data?.user_metadata.ten}</p>
+            </div>
+          </div>
+        </Flex>
 
-      <table style={tableStyles}>
-        <thead>
-          <tr style={rowStyles1}>
-            <th>Số thứ tự</th>
-            <th>Hành khách</th>
-            <th>Điểm đến</th>
+        <table style={tableStyles}>
+          <thead>
+            <tr style={rowStyles1}>
+              <th>Số thứ tự</th>
+              <th>Hành khách</th>
+              <th>Điểm đến</th>
 
-            <th>Giá vé</th>
-          </tr>
-        </thead>
-        <tbody>
-          {hanhkhach.map((item: any, key: any) => (
-            <tr style={rowStyles}>
-              <td>{key + 1}</td>
-              <td>{item.hoten}</td>
-              <td>{diemden}</td>
-              <td>${price}</td>
+              <th>Giá vé</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {hanhkhach.map((item: any, key: any) => (
+              <tr style={rowStyles}>
+                <td>{key + 1}</td>
+                <td>{item.hoten}</td>
+                <td>{diemden}</td>
+                <td>${price}</td>
+              </tr>
+            ))}
 
-          {/* Add more rows as needed */}
-          <tr style={lastRowStyles}>
-            <td
-              colSpan={3}
-              style={{
-                textAlign: "right",
-                marginTop: 50,
-                height: 80,
-              }}
-            >
-              <strong>Total:</strong>
-            </td>
-            <td>${hanhkhach.length * price}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+            {/* Add more rows as needed */}
+            <tr style={lastRowStyles}>
+              <td
+                colSpan={3}
+                style={{
+                  textAlign: "right",
+                  marginTop: 50,
+                  height: 80,
+                }}
+              >
+                <strong>Total:</strong>
+              </td>
+              <td>${hanhkhach.length * price}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
